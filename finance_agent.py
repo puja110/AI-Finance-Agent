99e49058cea5a3cf387e0c417f91468724cbae51
puja110 <csta.puja@gmail.com>
2025-01-26 12:32:41 -0500

    Added web_agent using the Groq model with DuckDuckGo for web-based queries

--- finance_agent.py
+++ finance_agent.py
@@ -1,7 +1,7 @@
 from phi.agent import Agent
 from phi.model.groq import Groq
-# from phi.model.openai import OpenAIChat
 from phi.tools.yfinance import YFinanceTools
+import phi.tools.yfinance import DuckDuckGo
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -32,7 +32,6 @@ def get_stock_symbol(company: str) -> str:
 
 agent  = Agent(
     model=Groq(id="llama-3.3-70b-versatile"),
-    # model=OpenAIChat(id="gpt-4o"),
     tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True), get_stock_symbol],
     show_tool_calls=True,
     markdown=True,
@@ -41,6 +40,15 @@ agent  = Agent(
         "If you need to find the symbol for a company, use the get_stock_symbol tool."
         ], # system prompt
     debug_mode=True
+)
+
+web_agent = Agent(
+    name="Web Agent",
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[DuckDuckGo()],
+    instructions=["Always include sources"],
+    show_tool_calls=True,
+    markdown=True
 )
 
 agent.print_response("Summarize and compare analyst recommendations and fundamentals for SPOT and CRM") # user prompt
